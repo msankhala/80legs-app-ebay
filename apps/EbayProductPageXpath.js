57d89ea4f8c019e3985894e8be41f84592688dac
Mahesh Sankhala <mahesh.saini@srijan.in>
2015-02-24 12:54:36 +0530

    apps/EbayProductPageXpath.js added

---
+++ apps/EbayProductPageXpath.js
@@ -0,0 +1,64 @@
+// This 80app returns the full HTML of each URL crawled
+function _x(STR_XPATH) {
+    var xresult = document.evaluate(STR_XPATH, document, null, XPathResult.ANY_TYPE, null);
+    var xnodes = [];
+    var xres;
+    while (xres = xresult.iterateNext()) {
+        xnodes.push(xres);
+    }
+
+    return xnodes;
+}
+
+var EightyApp = function() {
+  this.processDocument = function(html, url, headers, status, jQuery) {
+    // return html;
+    var app = this;
+    $ = jQuery;
+    var $html = app.parseHtml(html, $);
+    var object = {};
+
+    var products = [];
+    // $html.filter('#ListViewInner > li').each(function(i, obj) {
+    //  var product = {};
+    //  product.name = $(this).filter('.lvtitle a').text();
+    //  // product.url = $(this).filter('.lvtitle a').attr('href');
+    //  product.price = $(this).filter('.lvprice > span').clone().children().remove().end().text();
+    //  products.push(product);
+    // });
+    products.name = $(_x('//*[@id="itemTitle"]')).text().trim();
+    products.price = $(_x('//*[@id="prcIsum_bidPrice"]')).text().trim();
+    object.products = products;
+
+    return JSON.stringify(object);
+  };
+
+  this.parseLinks = function(html, url, headers, status, jQuery) {
+    var app = this;
+    var $ = jQuery;
+    var $html = app.parseHtml(html, $);
+    var links = [];
+
+    // gets all links in the html document
+    // $html.find('.lvtitle a').each(function(i, obj) {
+    //   var link = app.makeLink(url, $(this).attr('href'));
+    //   if(link !== null) {
+    //     links.push(link);
+    //   }
+    // });
+
+    return links;
+  };
+};
+
+try {
+  // Testing
+  module.exports = function(EightyAppBase) {
+    EightyApp.prototype = new EightyAppBase();
+    return new EightyApp();
+  };
+} catch(e) {
+  // Production
+  console.log("Eighty app exists.");
+  EightyApp.prototype = new EightyAppBase();
+}
